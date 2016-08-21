76f82c55e5c1ef1d35607f5851ff5cdbaa9592be
KartikRajaS <kartiksubu.krs@gmail.com>
2016-08-21 12:15:49 +0530

    attnd record service

--- server.js
+++ server.js
@@ -118,7 +118,7 @@ app.post('/getstudentsforattendancepickup',  urlencodedParser,function (req, res
    var tripid={"school_type":req.query.tripid};
    var schoolx={"school_id":req.query.schol};
      var route_id={"drop_route_id":req.query.routeid};
-   console.log(req.query.routeid);
+   //console.log(req.query.routeid);
    var query="SELECT p.student_id,(select student_name from student_details where id=p.student_id and school_id ='"+req.query.schol+"')as name from student_point p where school_id ='"+req.query.schol+"' and drop_route_id = (select id from route where route_name = '"+req.query.routeid+"' and school_id ='"+req.query.schol+"') and school_type ='"+req.query.tripid+"'";
    connection.query(query,
      function(err, rows){
@@ -154,7 +154,48 @@ app.post('/attsubmiturl',  urlencodedParser,function (req, res){
  });
 
 
+app.post('/addphoto',  urlencodedParser,function (req, res){
+            var data = req.query.x;
+            if (data.file) {
+                var path = data;
+                var file = fs.createWriteStream(path);
 
+                file.on('error', function (err) { 
+                    console.error(err) 
+                });
+
+                data.file.pipe(file);
+
+                data.file.on('end', function (err) { 
+                    var ret = {
+                        filename: data.file.hapi.filename,
+                        headers: data.file.hapi.headers
+                    }
+                    res(JSON.stringify(ret));
+                })
+            
+
+        }
+    
+});
+
+
+app.post('/attendancerecord' ,  urlencodedParser,function (req, res)
+{
+      var collection={"school_id":req.query.schol,"route_id":req.query.routeid,"mode_of_travel":req.query.pickupordrop,"trip":req.query.trip,"att_date":req.query.date,"status":req.query.status};//console.log(schoolx);
+      connection.query('insert into att_record set ?',[collection],
+        function(err, rows)
+        {
+      if(!err)
+      {
+      res.status(200).json({'returnval': 'success'});
+      }
+      else
+      {
+      res.status(200).json({'returnval': 'invalid'});
+      }
+});
+  });
 
 
 
